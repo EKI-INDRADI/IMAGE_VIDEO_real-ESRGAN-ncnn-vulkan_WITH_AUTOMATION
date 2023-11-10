5385ab7a336ec904d1264b1a14541083d6da494e
EKI-INDRADI <eki.indradi@gmail.com>
2023-11-10 09:00:13 +0700

    Update run_all_video_4x.bat

--- run_all_video_4x.bat
+++ run_all_video_4x.bat
@@ -1,30 +1,34 @@
 @echo off
 setlocal
 
-set video_input=onepiece_demo.mp4
+set video_format=mp4
+set video_input=onepiece_demo
 REM video_upscale=your_video.mp4
 set tmp_frames=tmp_frames
 set out_frames=out_frames
 set realesr_executable=realesrgan-ncnn-vulkan.exe
 set model_name=realesr-animevideov3
-set output_file=output_w_audio.mp4
-set fps_from_video=23.98
+
+set fps_from_video=29.97
 REM video_upscale=4 == 4x,3 == 3x,2 == 2x
-set fps_output_video=23.98
-set video_upscale=4
+set fps_output_video=29.97
+set video_upscale=2
 REM video_upscale=4 == 4x,3 == 3x,2 == 2x
-set use_gpu=0,1
-REM use_gpu=0,1 == MULTI GPU SUPPORT , use_gpu=0 == SINGLE GPU
+set use_gpu=1
+REM use_gpu=0,1 == MULTI GPU SUPPORT , use_gpu=0 == SINGLE GPU 1ST , use_gpu=1 == SINGLE GPU 2ND
 set pixel_format=yuv444p
 set cpu_threads=16
 set delay=1
 
+set output_file=%video_input%_%video_upscale%x_esrgan.%video_format%
+
 echo "--- VIDEO INFO"
-ffmpeg -i %video_input%
+ffmpeg -i %video_input%.%video_format%
 
 pause
 
 echo "--- ENV_INFO"
+echo "--- video_format=%video_format%"
 echo "--- video_input=%video_input%"
 echo "--- tmp_frames=%tmp_frames%"
 echo "--- out_frames=%out_frames%"
@@ -47,7 +51,7 @@ call run_folder_create.bat
 
 echo "--- Extract frames from the input video"
 :: Extract frames from the input video
-ffmpeg -i %video_input% -qscale:v 1 -qmin 1 -qmax 1 -r %fps_from_video% %tmp_frames%\frame%%08d.png
+ffmpeg -i %video_input%.%video_format% -qscale:v 1 -qmin 1 -qmax 1 -r %fps_from_video% %tmp_frames%\frame%%08d.png
 
 timeout /t %delay% /nobreak
 
@@ -59,7 +63,7 @@ timeout /t %delay% /nobreak
 
 echo "--- Reassemble enhanced frames with the original audio into a video"
 :: Reassemble enhanced frames with the original audio into a video
-ffmpeg -r %fps_from_video% -i %out_frames%\frame%%08d.png -i %video_input% -map 0:v:0 -map 1:a:0 -c:a copy -c:v libx264 -r %fps_output_video% -thread_queue_size %cpu_threads% -pix_fmt %pixel_format% %output_file%
+ffmpeg -r %fps_from_video% -i %out_frames%\frame%%08d.png -i %video_input%.%video_format% -map 0:v:0 -map 1:a:0 -c:a copy -c:v libx264 -r %fps_output_video% -thread_queue_size %cpu_threads% -pix_fmt %pixel_format% %output_file%
 
 timeout /t %delay% /nobreak
 
